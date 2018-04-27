7f9f6a5db97dc7af4d6de3e3985309c035813e26
Deimort <37375798+Deimort@users.noreply.github.com>
2018-04-27 09:55:24 +0200

    Bug

--- AI_ProvoostDylanStommenRomain_awale/src/awale/states/HumanVsHumanState.java
+++ AI_ProvoostDylanStommenRomain_awale/src/awale/states/HumanVsHumanState.java
@@ -21,7 +21,6 @@ public class HumanVsHumanState extends AwaleStates{
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawImage(ms.getImage(), 0, 0);
 		g.drawString("HVH", gc.getWidth()/2, gc.getHeight()/2);
 		
 	}
