afb87d8d37cb7308aadafd40d382e7b6704dbb78
Deimort <37375798+Deimort@users.noreply.github.com>
2018-05-18 09:29:35 +0200

    Fin de la javadoc d'Awale.domains

--- AI_ProvoostDylanStommenRomain_awale/src/awale/domains/Game.java
+++ AI_ProvoostDylanStommenRomain_awale/src/awale/domains/Game.java
@@ -15,6 +15,11 @@ public class Game {
 	private boolean isCycling;
 	private boolean turn;
 	
+	/**
+	 * Ce constructeur permet d'initialiser une partie
+	 * @param player1 le joueur 1
+	 * @param player2 le joueur 2
+	 */
 	public Game(Player player1,Player player2) {
 		players = new Player[] {player1,player2};
 		currentPlayer = players[0];
@@ -24,6 +29,12 @@ public class Game {
 		turn = false;
 	}
 	
+	/**
+	 * Ce constucteur permet d'initialiser une partie
+	 * @param player1 le joueur 1
+	 * @param player2 le joueur 2
+	 * @param board le plateau de départ
+	 */
 	public Game(Player player1,Player player2,int[][] board) {
 		players = new Player[] {player1,player2};
 		currentPlayer = players[0];
@@ -33,6 +44,11 @@ public class Game {
 		turn = false;
 	}
 	
+	/**
+	 * Cette méthode permet de donner une coordonée au joueur courant 
+	 * @param coord la coordonnée donnée
+	 * @return 1 si la coordonée est valide, 0 si elle ne l'est pas et -1 s'il n'y a pas de coordonnée possible
+	 */
 	public int giveCoord(Coordinate coord) {
 		return currentPlayer.setCurrentCoord(coord,currentBoard,checkStarvation());
 	}
@@ -45,6 +61,9 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Cette méthode permet de jouer un coup
+	 */
 	public void play() {
 		isCycling = !previousBoards.add(currentBoard);
 		
@@ -61,18 +80,34 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Cette méthode permet de déterminer s'il y a un cycle
+	 * @return true si c'est le cas, false sinon
+	 */
 	public boolean isCycling() {
 		return isCycling;
 	}
 	
+	/**
+	 * Cette méthode permet d'obtenir le plateau actuel
+	 * @return le plateau actuel
+	 */
 	public AwaleBoard getCurrentBoard() {
 		return currentBoard;
 	}
 	
+	/**
+	 * Cette méthode permet	d'obtenir l'id du joueur courant
+	 * @return id du joueur courant
+	 */
 	public int getCurrentPlayerID() {
 		return currentPlayer.getId();
 	}
 	
+	/**
+	 * Cette méthode permet d'obtenir le score des deux joueurs
+	 * @return un tableau contenant les scores des deux joueurs
+	 */
 	public int[] getScores() {
 		int[] scores = new int[2];
 		for(int i = 0; i < players.length; i++) {
@@ -82,8 +117,14 @@ public class Game {
 		return scores;
 	}
 	
+	/**
+	 * Cette méthode permet de déterminer le vainqueurde la partie
+	 * @return Joueur 1 si son score est plus élevé que celui du J2
+	 * Joueur2 si son score est plus élevé que celui du J1
+	 * Egalité autrement
+	 */
 	public String end() {
-		String winner = "Ã‰galitÃ©";
+		String winner = "Egalité";
 		setPlayerScore();
 		
 		if(players[0].getScore() > players[1].getScore()) {
@@ -95,6 +136,9 @@ public class Game {
 		return winner;
 	}
 
+	/**
+	 * Cette méthode permet de définir les scores de joueurs à la fin d'une partie
+	 */
 	private void setPlayerScore() {
 		for(int i = 0;i < 6;i++) {
 			players[0].setScore(currentBoard.getSeeds(players[0].getId(),i));
@@ -102,6 +146,10 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Cette méthode permet de déterminer si le joueur courant est en famine
+	 * @return true s'il l'est, false sinon
+	 */
 	public boolean starvationSelf() {
 		return currentBoard.checkStarvation(currentPlayer.getId());
 	}
--- AI_ProvoostDylanStommenRomain_awale/src/awale/states/Statistics.java
+++ AI_ProvoostDylanStommenRomain_awale/src/awale/states/Statistics.java
@@ -9,6 +9,9 @@ public class Statistics {
 	private int winnerId,gameCount;
 	private int[] victories;
 	
+	/**
+	 * Ceci est un constructeur de Statistics
+	 */
 	public Statistics() {
 		currentGameTime = 0;
 		totalTime = 0;
@@ -20,6 +23,11 @@ public class Statistics {
 		gameCount = 0;
 	}
 	
+	/**
+	 * Cette méthode permet d'obtenir les valeurs des différents attributs à la fin d'une partie
+	 * @param currentGameTime le temps de jeu actuel
+	 * @param scores les scores des deux joueurs
+	 */
 	public void endGame(int currentGameTime,int[] scores) {
 		this.currentGameTime = currentGameTime;
 		this.totalTime += currentGameTime;
@@ -27,13 +35,7 @@ public class Statistics {
 		setMaxTime(currentGameTime);
 		this.winnerId = getWinner(scores);
 		this.scores = scores;
-		incrementGames();
-	}
-
-	private void incrementGames() {
 		gameCount++;
-		
-		if(winnerId != -1)
 		victories[winnerId]++;
 	}
 
@@ -66,34 +68,70 @@ public class Statistics {
 		return String.format("%02d", seconds/60) + ":" + String.format("%02d", seconds%60);
 	}
 
+	/**
+	 * Cette méthode permet d'obtenir le temps de jeu actuel
+	 * @return le temps de jeu
+	 */
 	public String getCurrentGameTime() {
 		return getTime(currentGameTime);
 	}
 
+	/**
+	 * Cette méthode permet d'obtenir le temps de jeu minimum lors de cette utilisation
+	 * de l'application
+	 * @return le temps de jeu minimum
+	 */
 	public String getMinTime() {
 		return getTime(minTime);
 	}
 
+	/**
+	  * Cette méthode permet d'obtenir le temps de jeu maximum lors de cette utilisation
+	 * de l'application
+	 * @return
+	 */
 	public String getMaxTime() {
 		return getTime(maxTime);
 	}
-
+	
+	/**
+	 * Cette méthode permet d'obtenir le score pour un joueur donné
+	 * @param id l'id du joueur dont on souhaite obtenir le score
+	 * @return le score du joueur
+	 */
 	public int getScore(int id) {
 		return scores[id];
 	}
 
+	/**
+	 * Cette méthode permet d'obtenir l'id du joueur gagnant
+	 * @return l'id du joueur gagnant
+	 */
 	public int getWinnerId() {
 		return winnerId;
 	}
 
+	/**
+	 * Cette méthode permet d'obtenir le nombre de parties jouées lors de cette execution de l'application
+	 * @return le nombre de parties jouées lors de cette execution de l'application
+	 */
 	public int getGameCount() {
 		return gameCount;
 	}
 
+	/**
+	 * Cette méthode permet d'obtenir le nombre de parties gagnées lors de cette execution de l'application
+	 * pour un joueur donné
+	 * @return le nombre de parties gagnées lors de cette execution de l'application
+	 */
 	public int getVictories(int id) {
 		return victories[id];
 	}
 	
+	/**
+	 * Cette méthode permet d'obtenir le temps moyen d'une partie lors de cette execution de l'application
+	 * @return le temps moyen d'une partie lors de cette execution de l'application
+	 */
 	public String getAverageTime() {
 		String time;
 		try {
@@ -102,6 +140,7 @@ public class Statistics {
 			time = "00:00";
 		}
 		return time;
+		
 	}
 
 }
